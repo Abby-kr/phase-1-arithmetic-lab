47601585834d1c04697bb326bddb2f459e32ff41
Abby-kr <abby.a.otieno@gmail.com>
2023-07-10 17:53:45 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-
-
 let num1 = 31;
-let num2 = 2
-let multiply = num1 * num2
-let random = Math.floor(Math.random() * 10) + 1
-let num3 = 28
-let num4 = 6
-let mod = num3 % num4
-let max = Math.max(3,7,13,18,20)
+let num2 = 2;
+let multiply = num1 * num2;
+let random = Math.ceil(Math.random() * 10);
+let num3 = 28;
+let num4 = 6;
+let mod = num3 % num4;
+let max = Math.max(3,7,13,18,20);
 
